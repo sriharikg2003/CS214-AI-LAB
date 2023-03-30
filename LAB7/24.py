e208caccb669b07e783b7d6f0da1b909417efd5f
sriharikg2003 <sriharikg2003@gmail.com>
2023-03-30 18:06:02 +0530

    Code completed without Classes

--- LAB7/24.py
+++ LAB7/24.py
@@ -1,13 +1,6 @@
-"""
-Author :
-Srihari K G - 210030035
-Tejas - 210030039 
-Indian Institute of Technology Dharwad
-"""
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
 
 def chooseBestFeature(df):
@@ -24,6 +17,7 @@ def chooseBestFeature(df):
         ig_array[i] = getInfoGainOverFeature(df,i)
         
     return max(ig_array, key=ig_array.get),ig_array[max(ig_array, key=ig_array.get)]   
+
 
 def getInfoGainOverFeature(df,feature):
     
@@ -55,7 +49,7 @@ def getInfoGainOverFeature(df,feature):
         factor_array.append( ((len(dataframe_array[i])/total_values))*entropy_array[i] )
     
     return entropy_parent-sum(factor_array)
-    
+   
 
 def mylog(num):
     """
@@ -64,6 +58,8 @@ def mylog(num):
     if num!=0 :
         return np.log2(num)
     return 0
+
+
 
 
 def entropy(df):
@@ -82,25 +78,81 @@ def entropy(df):
     return -1*sum([(count[i]/total_Values)*mylog(count[i]/total_Values) for i in count.keys() ])
 
 
-def generateTree(df,depth):
+def generateTree(df, depth):
+    """
+    Returns a decision tree as a dictionary
+    """
+    if (len(df[df.columns[-1]].unique()) == 1) or (depth == 0) or (len(df.columns) == 1):
+        return (df[df.columns[-1]].mode())[0]
 
-    if (len(df[df.columns[-1]].unique())==1) or (depth==0):
-        return (df[df.columns[-1]].unique())[0]
-    
     else:
-        best_feature , info_gain = chooseBestFeature(df)
-        tree = {best_feature : {}}
-        
+        best_feature, info_gain = chooseBestFeature(df)
+        tree = {best_feature: {}}
+
         for i in df[best_feature].unique():
-            tree[best_feature][i] = generateTree(df[df[best_feature]==i],depth-1)
-        return tree   
+            tree[best_feature][i] = generateTree(df[df[best_feature] == i].drop(columns=[best_feature]), depth - 1)
+        return tree
+
+def predict(test, tree,arr):
+    """
+    Predicts an input instance to yes or no based on decision tree
+    """
+    if type(tree)==str:
+        return tree
+    
+    feature = list(tree.keys())[0]
+    if test[feature] in tree[feature]:
+        subtree = tree[feature][test[feature]]
+        return predict(test, subtree,arr)
+    else:
+        return arr[-1]
+
+def printTree(tree, gap=''):
+    """
+    Prints the decision tree in a readable format
+    """
+    if type(tree) == str:
+        print(tree)
+    else:
+        for feature, subtree in tree.items():
+            print(f'{gap}  {feature}:')
+            for value, subsubtree in subtree.items():
+                print(f'{gap*2}  {value} --> ', end='')
+                printTree(subsubtree, gap + "  ")
 
 
-if __name__ == '__main__':
+def accuracy(df):
+    """
+    Returns the accuracy percentage
+    """
+    predicted = []
+    for i in range(len(df)):
+        predicted.append(predict(df.iloc[i],tree,df[df.columns[-1]].unique()))
+    accuracy = 0
+    actual = np.array(df[df.columns[-1]])
+
+    accuracy = (sum(predicted == actual))*100/len(predicted)
+    print(f"Total instances          :   {len(predicted)}")
+    print(f"Correct Predicitions     :   {sum(predicted == actual)}")
+    print(f"Inorrect Predicitions    :   {len(predicted)-sum(predicted == actual)}")
+    print(f"Accuracy                 :   {accuracy} %")
+    return 
+
+
+
+
+
+
+if __name__ == "__main__":
+
     file_name = sys.argv[1]
     df = pd.read_table(file_name)
-    max_depth = 5
-    tree = generateTree(df,max_depth)
 
-    print("Decision Tree")
-    print(tree)
+    max_depth = 10
+
+    tree = generateTree(df,max_depth)
+    print("Decision tree  : \n")
+    printTree( tree)
+    accuracy(df)
+
+
